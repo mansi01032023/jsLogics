67888a8f49882c903cb2c315ebaa9c1bfbfc3c44
Mansi Bisaria <mansibisaria@cedcoss.com>
2023-03-14 17:54:45 +0530

    Initial commit

---
+++ src/JS/main.js
@@ -0,0 +1,35 @@
+const arr = [];
+function addProduct() {
+    // taking input from the user 
+    const pid = document.getElementById("prodId").value;
+    const pname = document.getElementById("prodName").value;
+    const pprice = document.getElementById("prodPrice").value;
+    // applying required validations
+    if (pid == "" || pname == "" || pprice == "") {
+        document.getElementById("msg").innerHTML = "Enter values for all fields";
+    } else if (!((pid >= 'A' && pid <= 'Z') || (pid >= 'a' && pid <= 'z') || (pid >= '0' && pid <= '9') || pid == "_")) {
+        document.getElementById("msg").innerHTML = "Enter valid ProductId";
+    } else if (!((pid >= 'A' && pid <= 'Z') || (pid >= 'a' && pid <= 'z') || (pid >= '0' && pid <= '9'))) {
+        document.getElementById("msg").innerHTML = "Enter valid ProductName";
+    } else if (!(pprice >= '0' && pprice <= '9')) {
+        document.getElementById("msg").innerHTML = "Enter valid ProductPrice";
+    } else {
+        arr.push({
+            ProductId: pid,
+            ProductName: pname,
+            ProductPrice: "USD" + " " + pprice
+        });
+        const tbl = document.getElementById("tbid");
+        const row = document.createElement("tr");
+        const c1 = document.createElement("td");
+        const c2 = document.createElement("td");
+        const c3 = document.createElement("td");
+        c1.innerHTML = arr[arr.length - 1].ProductId;
+        c2.innerHTML = arr[arr.length - 1].ProductName;
+        c3.innerHTML = arr[arr.length - 1].ProductPrice;
+        row.append(c1);
+        row.append(c2);
+        row.append(c3);
+        tbl.append(row);
+    }
+}
